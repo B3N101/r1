35f98c5143e6f263a627d66dd8b12fa794eb3dec
Benjamin Feuer <64800026+B3N101@users.noreply.github.com>
2023-01-04 22:14:30 -0500

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "next-themes";
 
 import localFont from "@next/font/local";
-import { Inter } from "@next/font/google";
-import { Merriweather } from "@next/font/google";
+import { Inter, Merriweather } from "@next/font/google";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -15,23 +14,26 @@ const inter = Inter({
 	variable: "--font-inter",
 });
 
+const merriweather = Merriweather({
+	subsets: ["latin"],
+	weight: "700",
+});
+
 export default function RootLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
 	return (
-    <html lang="en" className={inter.className}>
-      <head />
-      <body className="bg-white dark:bg-neutral-700 flex flex-col justify-between">
-        <ThemeProvider attribute="class">
-          <Navbar />
-          <main className="mb-auto">
-          {children}
-          </main>
-          <Footer />
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+		<html lang="en" className={inter.className}>
+			<head />
+			<body className="bg-white dark:bg-neutral-700 flex flex-col justify-between">
+				<ThemeProvider attribute="class">
+					<Navbar />
+					<main className="mb-auto">{children}</main>
+					<Footer />
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
--- next.config.js
+++ next.config.js
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-    fontLoaders: [
-      { loader: "@next/font/google", options: { subsets: ["latin"] } },
-    ],
-  },
-  reactStrictMode: true,
-  swcMinify: true,
+	experimental: {
+		appDir: true,
+		fontLoaders: [
+			{ loader: "@next/font/google", options: { subsets: ["latin"] } },
+		],
+	},
+	reactStrictMode: true,
+	modularizeImports: {
+		"@heroicons/react/24/solid": {
+			transform: "@heroicons/react/24/solid/{{member}}",
+		},
+	},
 };
 
 module.exports = nextConfig;
