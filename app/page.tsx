76cad1fc88c6ee2b0d87392456ff4fbcb0e55a22
Benjamin Feuer <64800026+B3N101@users.noreply.github.com>
2023-01-07 21:20:40 -0800

    Balancer added

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import text from "./components/text/text";
 import italic from "./components/text/italic";
+import Balancer from "react-wrap-balancer";
 import { Crimson_Pro } from "@next/font/google";
 const crimsonPro = Crimson_Pro({
 	subsets: ["latin"],
@@ -16,36 +17,57 @@ export default function Page() {
 			<p className="font-serif italic font-extrabold text-3xl">
 				asdfasfasfddas
 			</p>
-			
+
 			<text className={crimsonPro.className}>
 				<h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
 			</text>
 			<text>
 				<p className="text-xl">
-					This is a test of the emergency broadcast system. This is a test of
-					the emergency broadcast system. This is a test of the emergency
-					broadcast system. This is a test of the emergency broadcast system.
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at auctor
+					turpis. Aenean nec tellus id quam ultricies lacinia. Proin fringilla
+					ultrices dui non posuere. Duis eleifend venenatis mattis. Interdum et
+					malesuada fames ac ante ipsum primis in faucibus. Nunc sed nunc vel
+					diam tincidunt ultrices sed id neque. Class aptent taciti sociosqu ad
+					litora torquent per conubia nostra, per inceptos himenaeos. Sed sed
+					justo non ipsum porta euismod. Donec odio sapien, mattis vel ante et,
+					gravida molestie nisi. Maecenas efficitur ligula vitae vehicula
+					aliquam. Nullam ut leo at justo vulputate malesuada ac a augue. Nunc
+					vel tempor ipsum.
 				</p>
 			</text>
 			<text>
 				<h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
 			</text>
-			<text>
-				<p className="text-xl">
-					This is a test of the emergency broadcast system. This is a test of
-					the emergency broadcast system. This is a test of the emergency
-					broadcast system. This is a test of the emergency broadcast system.
-				</p>
+			<text className="text-xl">
+				<Balancer>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at auctor
+					turpis. Aenean nec tellus id quam ultricies lacinia. Proin fringilla
+					ultrices dui non posuere. Duis eleifend venenatis mattis. Interdum et
+					malesuada fames ac ante ipsum primis in faucibus. Nunc sed nunc vel
+					diam tincidunt ultrices sed id neque. Class aptent taciti sociosqu ad
+					litora torquent per conubia nostra, per inceptos himenaeos. Sed sed
+					justo non ipsum porta euismod. Donec odio sapien, mattis vel ante et,
+					gravida molestie nisi. Maecenas efficitur ligula vitae vehicula
+					aliquam. Nullam ut leo at justo vulputate malesuada ac a augue. Nunc
+					vel tempor ipsum.
+				</Balancer>
 			</text>
 			<text>
 				<h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
 			</text>
-			<text>
-				<p className="text-xl">
-					This is a test of the emergency broadcast system. This is a test of
-					the emergency broadcast system. This is a test of the emergency
-					broadcast system. This is a test of the emergency broadcast system.
-				</p>
+			<text className="text-xl">
+				<Balancer>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at auctor
+					turpis. Aenean nec tellus id quam ultricies lacinia. Proin fringilla
+					ultrices dui non posuere. Duis eleifend venenatis mattis. Interdum et
+					malesuada fames ac ante ipsum primis in faucibus. Nunc sed nunc vel
+					diam tincidunt ultrices sed id neque. Class aptent taciti sociosqu ad
+					litora torquent per conubia nostra, per inceptos himenaeos. Sed sed
+					justo non ipsum porta euismod. Donec odio sapien, mattis vel ante et,
+					gravida molestie nisi. Maecenas efficitur ligula vitae vehicula
+					aliquam. Nullam ut leo at justo vulputate malesuada ac a augue. Nunc
+					vel tempor ipsum.
+				</Balancer>
 			</text>
 			<text>
 				<h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
