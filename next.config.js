c90a123d5555894bb3a2244325e01fdf96503126
Benjamin Feuer <64800026+B3N101@users.noreply.github.com>
2023-02-05 12:21:23 -0500

    update next config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { withContentlayer } = require("next-contentlayer");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	experimental: {
@@ -12,6 +15,59 @@ const nextConfig = {
 			transform: "@heroicons/react/24/solid/{{member}}",
 		},
 	},
+	headers() {
+		return [
+			{
+				source: "/(.*)",
+				headers: securityHeaders,
+			},
+		];
+	}
 };
+// https://nextjs.org/docs/advanced-features/security-headers
+const contentSecurityPolicy = `
+  default-src 'self' vercel.live *.vercel.app *.vercel.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.vercel-insights.com vercel.live;
+  style-src 'self' 'unsafe-inline';
+  font-src 'self';
+  connect-src *;
+  media-src 'none';
+  img-src * blob: data:;
+`;
 
-module.exports = nextConfig;
+const securityHeaders = [
+	{
+		key: "Content-Security-Policy",
+		value: contentSecurityPolicy.replace(/\n/g, ""),
+	},
+	{
+		key: "Referrer-Policy",
+		value: "origin-when-cross-origin",
+	},
+	{
+		key: "X-Frame-Options",
+		value: "DENY",
+	},
+	{
+		key: "X-Content-Type-Options",
+		value: "nosniff",
+	},
+	{
+		key: "X-DNS-Prefetch-Control",
+		value: "on",
+	},
+	{
+		key: "X-DNS-Prefetch-Control",
+		value: "on",
+	},
+	{
+		key: "Strict-Transport-Security",
+		value: "max-age=31536000; includeSubDomains; preload",
+	},
+	{
+		key: "Permissions-Policy",
+		value: "camera=(), microphone=(), geolocation=()",
+	},
+];
+
+module.exports = withContentlayer(nextConfig);
