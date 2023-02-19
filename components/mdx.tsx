f3f64448f47d1f3576b4f7b199a317446977c03d
Benjamin Feuer <64800026+B3N101@users.noreply.github.com>
2023-02-19 13:17:22 -0500

    added news issue 1 22/3

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -7,11 +7,28 @@ type Props = {
 	code: string;
 };
 
+type PictureProps = {
+	src: string;
+	alt: string;
+	height: number;
+	width: number;
+	caption: string;
+};
+
+function Picture({ src, alt, height, width, caption }: PictureProps) {
+	return (
+		<div>
+			<Image src={src} alt={alt} height={height} width={width} />
+			<p className="text-center text-sm text-gray-500">{caption}</p>
+		</div>
+	);
+}
+
 export default function Mdx({ code }: Props) {
 	const MDXContent = useMDXComponent(code);
 	return (
 		<section className={clsx("prose", "dark:prose-invert")}>
-			<MDXContent components={{Link, Image}} />
+			<MDXContent components={{ Link, Image, Picture }} />
 		</section>
 	);
 }
